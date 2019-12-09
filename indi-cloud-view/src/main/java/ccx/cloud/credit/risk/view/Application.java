03af91505b26cb8c0a1c5b67bfb5f2487e0ed3f5
yangyifan <insight_ivan@163.com>
2019-12-09 13:49:35 +0800

    删除freemaker依赖

--- indi-cloud-view/src/main/java/ccx/cloud/credit/risk/view/Application.java
+++ indi-cloud-view/src/main/java/ccx/cloud/credit/risk/view/Application.java
@@ -1,14 +1,9 @@
 package ccx.cloud.credit.risk.view;
 
-import freemarker.template.Configuration;
-import freemarker.template.TemplateExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
@@ -20,18 +15,18 @@ public class Application {
 //		return new RestTemplate();
 //	}
 
-	@Bean
-	public Configuration configuration(){
-		Configuration cfg = new Configuration(Configuration.VERSION_2_3_28);
-//		String templatePath = FreemarkerUtil.class.getResource("/").getPath();
-//		cfg.setDirectoryForTemplateLoading(new File(templatePath));
-		cfg.setDefaultEncoding("UTF-8");
-		cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-		cfg.setLogTemplateExceptions(false);
-		cfg.setWrapUncheckedExceptions(true);
-
-		return cfg;
-	}
+//	@Bean
+//	public Configuration configuration(){
+//		Configuration cfg = new Configuration(Configuration.VERSION_2_3_28);
+////		String templatePath = FreemarkerUtil.class.getResource("/").getPath();
+////		cfg.setDirectoryForTemplateLoading(new File(templatePath));
+//		cfg.setDefaultEncoding("UTF-8");
+//		cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+//		cfg.setLogTemplateExceptions(false);
+//		cfg.setWrapUncheckedExceptions(true);
+//
+//		return cfg;
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
