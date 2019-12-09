a0818fb82cae42c250ddb7087028ad9cc4b0d73b
yangyifan <insight_ivan@163.com>
2019-12-09 13:41:18 +0800

    test

--- indi-cloud-view/src/main/java/ccx/cloud/credit/risk/view/controller/BaseController.java
+++ indi-cloud-view/src/main/java/ccx/cloud/credit/risk/view/controller/BaseController.java
@@ -16,6 +16,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+/**
+ *
+ */
 public class BaseController {
 
     @Resource
